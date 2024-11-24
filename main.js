96bd0d4c579d1fc3af65b27b23e701ca2027173e
AbdulTv <menblogerman8@gmail.com>
2024-11-24 12:11:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let names = prompt(`Ismingizni kiriting`)
+
+let familiya = prompt(`Familiyangizni kiriting`)
+
+let yoshi = prompt(`Necha yoshsiz?`)
+
+let username = prompt(`Instagramdagi user-name kiriting`)
+
+let savol1 = prompt(`Siz Mars ning qysi filialiga kelasiz?`)
+
+alert(`Xop endi siz javob berganlaringizni ko'rataman`)
+
+alert(`1) Sizni ismingiz: ${names}
+2) Sizni familiyangiz: ${familiya}
+3) Sizni yoshingiz: ${yoshi}
+4) Sizni instadagi username: ${username}
+5) Siz Mars ning: ${savol1} filialiga kelasiz`)
